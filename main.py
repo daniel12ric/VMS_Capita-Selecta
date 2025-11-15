c2bdae776785976c9263d2dec80cf00fd510e1d2
daniel <672022226@student.uksw.edu>
2025-11-15 21:25:37 +0700

    FINALE PROJECT Alfa

--- main.py
+++ main.py
@@ -99,6 +99,6 @@ def logout():
         session.pop('session_id', None)
     return redirect('/login')
 
-# run
+# runn
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
